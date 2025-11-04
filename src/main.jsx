5260badfec130277f4b799a9a096154f01cee190
John Moorman <45500240+mojoro@users.noreply.github.com>
2025-11-04 08:44:30 +0100

    Add mock functions and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+const router = createBrowserRouter([
+  { path: "/", element: <div>Hello world!</div> },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
