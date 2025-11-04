234287540be8a3a698d0720e1d8394adba4ae91e
John Moorman <45500240+mojoro@users.noreply.github.com>
2025-11-04 09:17:55 +0100

    Create error, contact pages and child route

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
+// first route is called the "root route"
 const router = createBrowserRouter([
-  { path: "/", element: <div>Hello world!</div> },
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "/contacts/:contactId", element: <Contact /> }],
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
